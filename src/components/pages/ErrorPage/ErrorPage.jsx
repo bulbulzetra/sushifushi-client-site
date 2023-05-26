81e5956ad01718df2c0feb14c568139b3c6cac9f
bulbul <bulbulzetra93@gmail.com>
2023-05-27 00:38:33 +0600

    error added

--- src/components/pages/ErrorPage/ErrorPage.jsx
+++ src/components/pages/ErrorPage/ErrorPage.jsx
@@ -7,7 +7,7 @@ const ErrorPage = () => {
     <section className="flex items-center h-screen p-16 bg-gray-100 text-gray-900">
       <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
         <div className="max-w-md text-center">
-          <h2 className="mb-8 font-extrabold text-9xl ">
+          <h2 className="mb-4 font-extrabold text-9xl ">
             <img src="https://i.postimg.cc/FsFybS21/5203299.jpg" alt="" />
           </h2>
           <p className="text-2xl text-orange-400 drop-shadow-xl font-semibold md:text-3xl mb-8">
@@ -17,7 +17,7 @@ const ErrorPage = () => {
             to="/"
             className="px-8 py-3 font-semibold rounded drop-shadow-xl bg-orange-300 hover:bg-orange-400 text-gray-900"
           >
-            Back to homepage
+            Back to the homepage
           </Link>
         </div>
       </div>
